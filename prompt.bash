a05d96927c1fb5cb2bdf29b208eae42ce81e4e0a
J. Federico Hernandez <fede.hernandez@gmail.com>
2015-08-31 15:00:17 -0300

    Removed 'git:' label from prompt. Changed color from gray to yellow.

--- prompt.bash
+++ prompt.bash
@@ -5,12 +5,14 @@ args=$@
 source "$dot/radar-base.sh"
 
 if is_repo; then
-  printf " \x01\033[1;30m\x02git:(\x01\033[0m\x02"
+  #printf " \x01\033[1;30m\x02git:\x01\033[1;33m\x02(\x01\033[0m\x02"
+  #printf " \x01\033[1;30m\x02git:(\x01\033[0m\x02"
+  printf " \x01\033[1;33m\x02(\x01\033[0m\x02"
   if show_remote_status $args; then
     bash_color_remote_commits
   fi
   readable_branch_name
   bash_color_local_commits
-  printf "\x01\033[1;30m\x02)\x01\033[0m\x02"
+  printf "\x01\033[1;33m\x02)\x01\033[0m\x02"
   bash_color_changes_status
 fi
